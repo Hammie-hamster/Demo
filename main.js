1b87ad98ec574610885203bb805132a218ade831
Hammie-hamster <75144189+Hammie-hamster@users.noreply.github.com>
2020-11-27 16:33:04 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+function text() {
+    document.getElementById("heading1").innerHTML = "So you found out what happens when you click on the text.";
+    document.getElementById("heading1").style.color = "aqua";
+}
